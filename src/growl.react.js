099b0e78a3d87daaaaa95c0597464028b690be46
George Lifchits <george.lifchits@gmail.com>
2015-04-22 22:26:30 -0700

    reindenting and converting tab to space

--- src/growl.react.js
+++ src/growl.react.js
@@ -10,158 +10,158 @@ var animations = true;
 var maxShown = 8;
 
 var movePosition = function() {
-	var y = position.slice(0, 1);
-	if(y == "t") {
-		holder.style.top = "0px";
-		holder.style.bottom = "auto";
-	} else {
-		holder.style.top = "auto";
-		holder.style.bottom = "0px";
-	}
+  var y = position.slice(0, 1);
+  if(y == "t") {
+    holder.style.top = "0px";
+    holder.style.bottom = "auto";
+  } else {
+    holder.style.top = "auto";
+    holder.style.bottom = "0px";
+  }
 
-	var x = position.slice(1, 2);
-	if(x == "l") {
-		holder.style.left = "0px";
-		holder.style.right = "auto";
-	} else if(x == "r") {
-		holder.style.left = "auto";
-		holder.style.right = "0px";
-	} else {
-		var neg = holder.clientWidth / 2;
-		var left = (window.innerWidth / 2) - neg;
-		holder.style.left = left + "px";
-		holder.style.right = "auto";
-	}
+  var x = position.slice(1, 2);
+  if(x == "l") {
+    holder.style.left = "0px";
+    holder.style.right = "auto";
+  } else if(x == "r") {
+    holder.style.left = "auto";
+    holder.style.right = "0px";
+  } else {
+    var neg = holder.clientWidth / 2;
+    var left = (window.innerWidth / 2) - neg;
+    holder.style.left = left + "px";
+    holder.style.right = "auto";
+  }
 }
 
 var Growl = React.createClass({
 
-	// This is just a counter, don't modify directly
-	uid: 5200,
+  // This is just a counter, don't modify directly
+  uid: 5200,
 
-	levels: ['info', 'warn', 'error', 'success'],
+  levels: ['info', 'warn', 'error', 'success'],
 
-	// Convenience constans for setting notification level
-	WARN: 'warn',
-	INFO: 'info',
-	ERROR: 'error',
-	SUCCESS: 'success',
+  // Convenience constans for setting notification level
+  WARN: 'warn',
+  INFO: 'info',
+  ERROR: 'error',
+  SUCCESS: 'success',
 
-	// Use these statics to configure all Growls from anywhere in your application
-	statics: {
-		setPosition: function(pos) {
-			if(inArray(pos, valid_positions)) {
-				position = pos;
-			} else {
-				console.log('Unknown position supplied.');
-			}
+  // Use these statics to configure all Growls from anywhere in your application
+  statics: {
+    setPosition: function(pos) {
+      if(inArray(pos, valid_positions)) {
+        position = pos;
+      } else {
+        console.log('Unknown position supplied.');
+      }
 
-			if(holder !== null) {
-				movePosition();
-			}
-		},
-		setMaxToShow: function(ct) {
-			maxShown = ct;
-		},
-		setDelay: function(del) {
-			delay = parseInt(del);
-			SingleGrowl.setDelay(del);
-		},
-		getDelay: function() {
-			return delay;
-		},
-		noAnimations: function() {
-			animations = false;
-			SingleGrowl.noAnimations();
-		}		
-	},
+      if(holder !== null) {
+        movePosition();
+      }
+    },
+    setMaxToShow: function(ct) {
+      maxShown = ct;
+    },
+    setDelay: function(del) {
+      delay = parseInt(del);
+      SingleGrowl.setDelay(del);
+    },
+    getDelay: function() {
+      return delay;
+    },
+    noAnimations: function() {
+      animations = false;
+      SingleGrowl.noAnimations();
+    }
+  },
 
-	getInitialState: function() {
-		return {
-			notifications: []
-		}
-	},
+  getInitialState: function() {
+    return {
+      notifications: []
+    }
+  },
 
-	getDefaultProps: function() {		
-		return {};
-	},	
+  getDefaultProps: function() {
+    return {};
+  },
 
-	handleRemovedNotification: function(uid) {
-		var notifications = this.state.notifications;
-		var n = notifications.filter(function(ele) {
-			return ele.uid !== uid;
-		});
-		this.setState( { notifications: n} );
-	},
+  handleRemovedNotification: function(uid) {
+    var notifications = this.state.notifications;
+    var n = notifications.filter(function(ele) {
+      return ele.uid !== uid;
+    });
+    this.setState( { notifications: n} );
+  },
 
-	addNotification: function(note) {
-		var n = this.state.notifications;
-		var self = this;
-		try {
-			if(note.level) {
-				if(!inArray(note.level, this.levels)) {
-					throw "Invalid level supplied";
-				} else {
-					note.uid = this.uid;
-					note.ref = "notification-"+this.uid;
-					this.uid += 1;
-					note.timeout = false;
+  addNotification: function(note) {
+    var n = this.state.notifications;
+    var self = this;
+    try {
+      if(note.level) {
+        if(!inArray(note.level, this.levels)) {
+          throw "Invalid level supplied";
+        } else {
+          note.uid = this.uid;
+          note.ref = "notification-"+this.uid;
+          this.uid += 1;
+          note.timeout = false;
 
-					n.push(note);
-					
-					this.setState({ notifications: n });
-				}
-			}
-		} catch (ex) {
-			console.log('Error adding notification: '+ex);
-		}
-	},
+          n.push(note);
 
-	componentDidMount: function() {
-		if(holder === null) {
-			holder = this.getDOMNode();
-		}
-		movePosition();
-	},
+          this.setState({ notifications: n });
+        }
+      }
+    } catch (ex) {
+      console.log('Error adding notification: '+ex);
+    }
+  },
 
-	render: function() {
-		var that = this;
+  componentDidMount: function() {
+    if(holder === null) {
+      holder = this.getDOMNode();
+    }
+    movePosition();
+  },
 
-		if(this.state.notifications.length == 0 ) {
-			return <div className="growl-wrapper empty"></div>;
-		}
-		var isMore = "";
-		var count = 0;
-		if(this.state.notifications.length > maxShown) {
-			var amt = this.state.notifications.length - maxShown;
-			isMore = <li key="more-still"><span>{amt} more</span></li>
-		}
+  render: function() {
+    var that = this;
 
-		return (
-			<div className="growl-wrapper">
-			  <ul>
-				{this.state.notifications.map(function(n) {					
-					count += 1;
-					if(count >= maxShown) {
-						return "";
-					} else {
-						return <SingleGrowl key={n.uid} ref={n.ref} notification={n} onDidRemove={that.handleRemovedNotification} />
-					}
-				})}
-				{isMore}
-			  </ul>
-			</div>
-		);
-	
-	}
+    if(this.state.notifications.length == 0 ) {
+      return <div className="growl-wrapper empty"></div>;
+    }
+    var isMore = "";
+    var count = 0;
+    if(this.state.notifications.length > maxShown) {
+      var amt = this.state.notifications.length - maxShown;
+      isMore = <li key="more-still"><span>{amt} more</span></li>
+    }
+
+    return (
+      <div className="growl-wrapper">
+      <ul>
+      {this.state.notifications.map(function(n) {
+        count += 1;
+        if(count >= maxShown) {
+          return "";
+        } else {
+          return <SingleGrowl key={n.uid} ref={n.ref} notification={n} onDidRemove={that.handleRemovedNotification} />
+        }
+      })}
+      {isMore}
+      </ul>
+      </div>
+    );
+
+  }
 });
 
 function inArray(needle, haystack) {
-	var length = haystack.length;
-	for(var i=0; i < length; i++) {
-		if(haystack[i] == needle) return true;
-	}
-	return false;
+  var length = haystack.length;
+  for(var i=0; i < length; i++) {
+    if(haystack[i] == needle) return true;
+  }
+  return false;
 }
 
 module.exports = Growl;
--- src/single-growl.react.js
+++ src/single-growl.react.js
@@ -5,101 +5,101 @@ var delay = 3000;
 
 var SingleGrowl = React.createClass({
 
-	getInitialState: function() {
-		return {
-			remove: false
-		};
-	},
+  getInitialState: function() {
+    return {
+      remove: false
+    };
+  },
 
-	setRemove: function() {
+  setRemove: function() {
 
-		// Just in case this was triggered some other way than the timeout itself.
-		clearTimeout(this.props.notification.timeout);
+    // Just in case this was triggered some other way than the timeout itself.
+    clearTimeout(this.props.notification.timeout);
 
-		if(!animations) {
-			this.props.onDidRemove(this.props.notification.uid);
-		} else {
-			this.setState({ remove: true });
-		}
-	},
+    if(!animations) {
+      this.props.onDidRemove(this.props.notification.uid);
+    } else {
+      this.setState({ remove: true });
+    }
+  },
 
-	statics: {
-		noAnimations: function() {
-			animations = false;
-		},
-		setDelay: function(ms) {
-			delay = ms;
-		},
-		getDelay: function() {
-			return delay;
-		}
-	},
-	
-	getDefaultProps: function() {
-		return {
-			notification: null,
-			onDidRemove: function(uid) {}
-		};
-	},
+  statics: {
+    noAnimations: function() {
+      animations = false;
+    },
+    setDelay: function(ms) {
+      delay = ms;
+    },
+    getDelay: function() {
+      return delay;
+    }
+  },
 
-	startTimer: function() {
-		var note = this.props.notification;
-		var self = this;
-		note.timeout = setTimeout(function() {
-						self.setRemove();
-					}, delay);
-	},
+  getDefaultProps: function() {
+    return {
+      notification: null,
+      onDidRemove: function(uid) {}
+    };
+  },
 
-	componentDidMount: function() {
-		// This should always evaluate to true, but just in case...
-		if(this.props.notification.timeout === false) {
-			this.startTimer();
-		}
+  startTimer: function() {
+    var note = this.props.notification;
+    var self = this;
+    note.timeout = setTimeout(function() {
+      self.setRemove();
+    }, delay);
+  },
 
-		if(animations) {
-			var self = this;
-			var ele = this.getDOMNode();
-			var transitionEvent = whichTransitionEvent();
-			if(transitionEvent) {
-				ele.addEventListener(transitionEvent, function() {
-					if(self.state.remove) {
-						self.props.onDidRemove(self.props.notification.uid);
-					}
-				});
-			} else {
-				// Force animations to false bc this browser doesn't support them... 
-				console.log('Animations disabled. Browser does not support.');
-				animations = false;
-			}
-		}
-	},
+  componentDidMount: function() {
+    // This should always evaluate to true, but just in case...
+    if(this.props.notification.timeout === false) {
+      this.startTimer();
+    }
 
-	render: function() {
-		var cname = "growl " + this.props.notification.level;
-		if(this.state.remove) {
-			cname = cname + " removing";
-		}
-		return (<li className={cname}><span>[{this.props.notification.level}] {this.props.notification.msg}</span></li>);
-	}
+    if(animations) {
+      var self = this;
+      var ele = this.getDOMNode();
+      var transitionEvent = whichTransitionEvent();
+      if(transitionEvent) {
+        ele.addEventListener(transitionEvent, function() {
+          if(self.state.remove) {
+            self.props.onDidRemove(self.props.notification.uid);
+          }
+        });
+      } else {
+        // Force animations to false bc this browser doesn't support them...
+        console.log('Animations disabled. Browser does not support.');
+        animations = false;
+      }
+    }
+  },
+
+  render: function() {
+    var cname = "growl " + this.props.notification.level;
+    if(this.state.remove) {
+      cname = cname + " removing";
+    }
+    return (<li className={cname}><span>[{this.props.notification.level}] {this.props.notification.msg}</span></li>);
+  }
 
 });
 
 /* From Modernizr */
 function whichTransitionEvent(){
-    var t;
-    var el = document.createElement('fakeelement');
-    var transitions = {
-      'transition':'transitionend',
-      'OTransition':'oTransitionEnd',
-      'MozTransition':'transitionend',
-      'WebkitTransition':'webkitTransitionEnd'
-    }
+  var t;
+  var el = document.createElement('fakeelement');
+  var transitions = {
+    'transition':'transitionend',
+    'OTransition':'oTransitionEnd',
+    'MozTransition':'transitionend',
+    'WebkitTransition':'webkitTransitionEnd'
+  }
 
-    for(t in transitions){
-        if( el.style[t] !== undefined ){
-            return transitions[t];
-        }
+  for(t in transitions){
+    if( el.style[t] !== undefined ){
+      return transitions[t];
     }
+  }
 }
 
 module.exports = SingleGrowl;
